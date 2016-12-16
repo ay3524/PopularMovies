3f925f8b91975c6dcfe669186cf9cb6887de2c99
Ashish Yadav <ay3524@gmail.com>
2016-12-17 02:39:02 +0530

    Initial commit

---
+++ app/src/main/java/ay3524/com/popularmovies/api/ApiClient.java
@@ -0,0 +1,25 @@
+package ay3524.com.popularmovies.api;
+
+/**
+ * Created by Ashish on 10-11-2016.
+ */
+
+import ay3524.com.popularmovies.Utils.Constants;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+
+public class ApiClient {
+
+    private static Retrofit retrofit = null;
+
+    public static Retrofit getClient() {
+        if (retrofit==null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(Constants.BASE_URI)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}
